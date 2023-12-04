ceac3a7376b710b2830dc14bc45921e681dce819
LU ! <152495895+portygul@users.noreply.github.com>
2023-12-04 15:11:21 +0100

    Update types.ts
    remove interface that cant be reused lol

--- src/Reuseable/types.ts
+++ src/Reuseable/types.ts
@@ -9,9 +9,3 @@ export const numberRegex: RegExp = /[0-9]+/g;
 export const numberRegexGlobal: RegExp = /[0-9]+/;
 
 export const digitWordArray : string[] = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
-export interface BallGameMinSpecs {
-    id: number,
-    red: number,
-    blue: number,
-    green: number
-}
